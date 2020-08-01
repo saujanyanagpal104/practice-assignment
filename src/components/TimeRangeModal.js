d3bf92d1004602f0c430fa2096fa5636e8514174
saujanyanagpal104 <saujanyanagpal104@gmail.com>
2020-08-01 23:05:42 +0530

    react-heroku

--- src/components/TimeRangeModal.js
+++ src/components/TimeRangeModal.js
@@ -25,11 +25,11 @@ const TimeRangeModel = ({activityPeriods, name, toggleModal}) => {
                 <span className='modal-window-heading'>Activity Time's of <span className='modal-window-name'>{name}</span></span>
                 <Calender setFilterDate={setFilterDate} />
                 <div className='activity-shifts'>
-                    {activityShifts.map((shift, index) => (
+                    {activityShifts.length ? activityShifts.map((shift, index) => (
                         <span key={index} className='shift-range'>
                             {shift.start_time} - {shift.end_time}
                         </span>
-                    ))}
+                    )) : <span className='no-shifts'>No Shifts!</span>}
                 </div>
             </div>
         </div>
